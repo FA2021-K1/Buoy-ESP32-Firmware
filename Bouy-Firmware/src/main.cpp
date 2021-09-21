37a35f0a57e32e9e951fc7b831eeb6831d0261c4
Patrick <ps.schmidt@tum.de>
2021-09-21 10:48:56 +0200

    include all headers for testing compilation

--- Bouy-Firmware/src/main.cpp
+++ Bouy-Firmware/src/main.cpp
@@ -1,7 +1,18 @@
 #include <Arduino.h>
+
+#include "bluetooth.h"
+#include "buoy.h"
+#include "global_enums.h"
+#include "gpssensor.h"
+#include "hardwareLayout.h"
 #include "rasppi.h"
-// #include "buoy.h"
-// #include "SensorData.h"
+#include "sdcard.h"
+#include "sensor.h"
+#include "sensors/analogsensor.h"
+#include "sensordata.h"
+#include "transferdata.h"
+#include "value.h"
+
 
 void setup() {
   // put your setup code here, to run once:

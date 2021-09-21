16279a5425b398560d53a8b7a62e017dfc0c0120
Patrick <ps.schmidt@tum.de>
2021-09-21 10:52:22 +0200

    test for serial logging

--- Bouy-Firmware/src/main.cpp
+++ Bouy-Firmware/src/main.cpp
@@ -16,9 +16,12 @@
 
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(115200);
   Serial.println("LEN= ");
 }
 
 void loop() {
+  Serial.println("hi");
+  delay(1000);
   // put your main code here, to run repeatedly:
 }

73fe07fa8ba3a78b5e6fe75959bc92447e7935d4
Patrick <ps.schmidt@tum.de>
2021-09-22 19:41:37 +0200

    Add mock function manager::execute() for demo

--- Bouy-Firmware/include/manager.h
+++ Bouy-Firmware/include/manager.h
@@ -6,19 +6,25 @@
 #include "buoy.h"
 #include "rasppi.h"
 #include "sdcard.h"
+#include "sensors/tdssensor.h"
+#include "gpssensor.h"
 
 
 class Manager {
 public:
   Manager() {}
   void create_objects();
+  void execute();
   std::shared_ptr<Buoy> get_buoy() {return _buoy;}
   std::shared_ptr<RaspPi> get_rasppi() {return _rasppi;}
   std::shared_ptr<SDCard> get_sdcard() {return _sdcard;}
+  std::shared_ptr<GPSSensor> get_gpssensor() {return _gpssensor;}
 private:
   std::shared_ptr<Buoy> _buoy;
   std::shared_ptr<RaspPi> _rasppi;
   std::shared_ptr<SDCard> _sdcard;
+  std::shared_ptr<GPSSensor> _gpssensor;
+  std::shared_ptr<TDSSensor> _tdssensor;
 };
 
 #endif
--- Bouy-Firmware/src/manager.cpp
+++ Bouy-Firmware/src/manager.cpp
@@ -1,12 +1,31 @@
 #include <memory>
 
+#include "manager.h"
 #include "buoy.h"
 #include "rasppi.h"
-#include "manager.h"
+#include "sensordata.h"
+#include "value.h"
+#include "sensors/analogsensor.h"
 
 void Manager::create_objects() {
     _buoy = std::make_shared<Buoy>(1234);
+    _tdssensor = std::make_shared<TDSSensor>(15);
     _rasppi = std::make_shared<RaspPi>();
     _sdcard = std::make_shared<SDCard>();
+    _gpssensor = std::make_shared<GPSSensor>();
+    
     _sdcard->init();
+    _gpssensor->init();
+}
+
+void Manager::execute() {
+    // todo turn on relay
+
+    Location location = _gpssensor->getLocation();
+    DateTime datetime = _gpssensor->getDateTime();
+    std::vector<Value> values;
+    values.emplace_back(2, 1, _tdssensor->get_ppm_value());
+    SensorData sensordata(_buoy, location, datetime.to_iso(), values);
+
+    // todo send to raspy
 }

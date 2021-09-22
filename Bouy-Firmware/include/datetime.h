0d5836ec902f51d6058bcfc7ee089a92066f8f63
Patrick <ps.schmidt@tum.de>
2021-09-22 19:35:10 +0200

    Add string conversion functions for DateTime

--- Bouy-Firmware/include/datetime.h
+++ Bouy-Firmware/include/datetime.h
@@ -1,7 +1,16 @@
+#ifndef DATETIME_H
+#define DATETIME_H
+
+#include <Arduino.h>
+
 #include <cstdint>
+
 
 class DateTime {
     public:
+        DateTime();
+        DateTime(uint16_t, uint8_t, uint8_t, uint8_t, uint8_t, uint8_t, uint8_t);
+
         uint16_t year;
         uint8_t month;
         uint8_t day;
@@ -9,4 +18,9 @@ class DateTime {
         uint8_t minute;
         uint8_t second;
         uint8_t centisecond;
+
+        std::string to_iso();
+        std::string to_filename();
 };
+
+#endif
---
+++ Bouy-Firmware/src/datetime.cpp
@@ -0,0 +1,27 @@
+#include <cstdio>
+
+#include "datetime.h"
+
+DateTime::DateTime (uint16_t year_, uint8_t month_, uint8_t day_,
+                    uint8_t hour_, uint8_t minute_, uint8_t second_,
+                    uint8_t centisecond_) : year(year_), month(month_),
+                    day(day_), hour(hour_), minute(minute_), second(second_),
+                    centisecond(centisecond_) {}
+
+std::string DateTime::to_iso() {
+  unsigned int string_length = 21;
+  char string_buffer[string_length];
+  snprintf(string_buffer, string_length, "%04u-%02u-%02uT%02u:%02u:%02uZ",
+          static_cast<uint>(year), static_cast<uint>(month), static_cast<uint>(day),
+          static_cast<uint>(hour), static_cast<uint>(minute), static_cast<uint>(second));
+  return string_buffer;
+}
+
+std::string DateTime::to_filename() {
+  unsigned int string_length = 20;
+  char string_buffer[string_length];
+  snprintf(string_buffer, string_length, "%04u-%02u-%02u_%02u-%02u-%02u",
+          static_cast<uint>(year), static_cast<uint>(month), static_cast<uint>(day),
+          static_cast<uint>(hour), static_cast<uint>(minute), static_cast<uint>(second));
+  return string_buffer;
+}

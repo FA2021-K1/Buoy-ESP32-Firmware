4f31e940d0840e1626d91f66cfdfd164b4e5224c
surajraob0108 <surajraob@gmail.com>
2021-09-22 17:34:18 +0200

    Sensor header files added

---
+++ Bouy-Firmware/include/sensors/phsensor.h
@@ -0,0 +1,16 @@
+/*
+ * Interface for the TDS Sensor (Conductivity)
+ * Provides funcitons and structs to initialize the TDS Sensor
+ * and retrieve data
+ */
+#ifndef PHSENSOR_H
+#define PHSENSOR_H
+#include "analogsensor.h"
+
+class PHSensor : public AnalogSensor
+{
+    public:
+
+    private:
+};
+#endif
---
+++ Bouy-Firmware/include/sensors/tdssensor.h
@@ -0,0 +1,16 @@
+/*
+ * Interface for the TDS Sensor (Conductivity)
+ * Provides funcitons and structs to initialize the TDS Sensor
+ * and retrieve data
+ */
+#ifndef TDSSENSOR_H
+#define TDSSENSOR_H
+#include "analogsensor.h"
+
+class TDSSensor : public AnalogSensor
+{
+    public:
+
+    private:
+};
+#endif

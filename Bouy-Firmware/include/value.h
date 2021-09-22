85d28ef3f58945238b7de041124eef014b030a5f
Patrick <ps.schmidt@tum.de>
2021-09-22 14:41:16 +0200

    add constructor for Value

--- Bouy-Firmware/include/value.h
+++ Bouy-Firmware/include/value.h
@@ -4,6 +4,9 @@
 #include <cstdint>
 
 class Value {
+public:
+  Value(uint32_t type_id_, uint32_t sensor_id_, double data_) : 
+    type_id(type_id_), sensor_id(sensor_id_), data(data_) {};
   uint32_t type_id;
   uint32_t sensor_id;
   double data;

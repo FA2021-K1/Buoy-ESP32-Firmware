87afe9783597d3d9df74c35d1a7884fbd0949d02
Noa16h <n.dormann@gmx.net>
2021-09-25 15:54:03 +0200

    lora headers second iteration

--- Bouy-Firmware/include/lora_header/buoy_header.h
+++ Bouy-Firmware/include/lora_header/buoy_header.h
@@ -10,44 +10,53 @@ struct buoy_header
     uint8_t next_header;
 };
 
+/* BTCP Header ----------------------------------------------*/
+#define BTCP_HEADER_ID 0x06
+
 struct btcp_header
 {
     uint8_t seq_number;
     uint8_t ack_number;
-    union{
-        uint8_t raw_flags;
-        struct
-        {
-            uint8_t ack : 1,
-                    syn : 1,
-                    rst : 1,
-                    fin : 1,
-                    reserved : 4;
-        };        
-    };
     uint8_t flags;
     uint8_t next_header;
 };
 
+#define BTCP_FLAG_ACK 0b00000001
+#define BTCP_FLAG_SYN 0b00000010
+#define BTCP_FLAG_RST 0b00000100
+#define BTCP_FLAG_FIN 0b00001000
+
+
+/* Metadata Header ----------------------------------------------*/
+#define METADATA_HEADER_ID 0x10
+
 struct metadata_header
 {
-    struct
-    {
-        uint8_t more_data : 1,
-                size      : 7;
-    };
+    uint8_t size;
     uint8_t next_header;
 };
 
+#define METADATA_FLAG_MD 0b10000000
+#define MEASUREMENT_FLAG_MD 0b10000000
+
+
+/* Measurement Header ----------------------------------------------*/
+#define MEASUREMENT_HEADER_ID 0x11
+
+#define METADATA_SIZE_MASK 0b01111111
+#define MEASUREMENT_SIZE_MASK 0b01111111
+
 typedef metadata_header measurement_header;
 
-struct meta_data_payload_field
+/* Metadata payload field ----------------------------------------------*/
+struct metadata_payload_field
 {
     uint16_t buoy_id;
     uint32_t latest_measurement_id;
     uint32_t oldest_measurement_id; //has not been saved yet
 };
 
+/* Measurement payload special field ----------------------------------------------*/
 struct measurement_payload_special_field
 {
     uint16_t buoy_id;
@@ -56,6 +65,7 @@ struct measurement_payload_special_field
     uint32_t timestamp; //unix time
 };
 
+/* Measurement payload value field ----------------------------------------------*/
 struct measurement_payload_value_field
 {
     uint8_t sensor_id;

8e150ecc20f9fd4fdd358cc8e459623b2927a38a
Noa16h <n.dormann@gmx.net>
2021-09-26 09:28:57 +0200

    added layer 5 header

---
+++ Bouy-Firmware/include/lora_header/data_exchange_protocol.h
@@ -0,0 +1,21 @@
+#ifndef DATA_EXCHANGE_PROTOCOL
+#define DATA_EXCHANGE_PROTOCOL
+
+#include <memory>
+#include <lora_header/btcp.h>
+
+enum DataProtocolState
+{
+    NEW,
+    WAITING,
+    METADATA_EXCHANGE,
+    MEASUREMENTS_EXCHANGE
+};
+
+struct remote_buoy
+{
+    std::shared_ptr<struct btcp_state> btcp_state;
+    DataProtocolState prot_state;
+};
+
+#endif

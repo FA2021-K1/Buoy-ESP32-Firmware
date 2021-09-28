77432553105143fab8c8834641d6792878bbdb16
Patrick <ps.schmidt@tum.de>
2021-09-28 10:14:05 +0200

    logging

--- Bouy-Firmware/src/buoy.cpp
+++ Bouy-Firmware/src/buoy.cpp
@@ -1,8 +1,12 @@
 #include <memory>
 
+#include <Arduino.h>
+
 #include "buoy.h"
 
-Buoy::Buoy(uint16_t buoy_id) : _buoy_id(buoy_id) {}
+Buoy::Buoy(uint16_t buoy_id) : _buoy_id(buoy_id) {
+  Serial.printf("Buoy created with id %d\n", _buoy_id);
+}
 
 void Buoy::attachSensor(std::shared_ptr<Sensor> sensor) {
   _sensors.push_back(sensor);
@@ -10,8 +14,10 @@ void Buoy::attachSensor(std::shared_ptr<Sensor> sensor) {
 
 std::vector<Value> Buoy::sampleAllSensors() {
   std::vector<Value> all_values;
+  Serial.println("Now sampling all sensors");
   for (auto sensor : _sensors) {
     auto sensor_values = sensor->sampleValues();
+    Serial.printf("From sensor %d sampled %f (only printing first value) \n", sensor->get_sensor_id(), sensor_values[0]);
     all_values.insert(all_values.end(), sensor_values.begin(), sensor_values.end());
   }
   return all_values;

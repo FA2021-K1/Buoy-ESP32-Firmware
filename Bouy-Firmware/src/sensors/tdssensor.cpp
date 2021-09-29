26e6e6831fb58ec24353d50f679daa7796155c2e
Patrick <ps.schmidt@tum.de>
2021-09-29 08:49:16 +0200

    add logging

--- Bouy-Firmware/src/sensors/tdssensor.cpp
+++ Bouy-Firmware/src/sensors/tdssensor.cpp
@@ -13,10 +13,12 @@ TDSSensor::TDSSensor(uint8_t sensor_id) : Sensor(sensor_id, SensorType::COND) {
 }
 
 std::vector<Value> TDSSensor::sampleValues() {
+  Serial.printf("Sampling sensor %d\n", _sensor_id);
   double adcValue = analogRead(TDS_PIN);
   double voltageValue = (adcValue / 4096) * 2.3;
   double ppmValue = (voltageValue / 2.3) * 1000;
   std::vector<Value> values;
   values.emplace_back(_type_id, _sensor_id, ppmValue);
+  Serial.printf("Sensor returned values %f\n", values[0].data);
   return values;
 }
